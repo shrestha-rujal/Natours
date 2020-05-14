192acf08ef2877fa690a39e087007b62731366b9
Rujal Shrestha <rujal4shrz@gmail.com>
2020-05-15 00:03:56 +0545

    Mongoose DB connection with MVC file restructure && refresh script
    - [x] Setup connection with mongodb atlas and initialize mongoose
    - [x] Create model/schema for Tour and test creating tour doc
    - [x] Create MVC file structre, remove fs module and related code, add createTour through mongodb
    - [x] Add Read, Read single, update and delete tour apis
    - [x] Update Tour model with more properties
    - [x] Add script to refresh tours data

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
+
+const DB_URI = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
 const PORT = 3000;
+
+mongoose.connect(DB_URI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+})
+  .then(() => console.log('DATABASE CONNECTION SUCCESSFUL'))
+  .catch((err) => console.error('Database connection failed: ', err));
 
 app.listen(process.env.PORT || PORT, () => {
   console.log(`SERVER LISTENING AT ${PORT}`);
