02a670bffa7f629cbdbc4f113103d3263fa5956d
Rujal Shrestha <rujal4shrz@gmail.com>
2020-06-16 20:00:53 +0545

    Create signup user token

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -1,11 +1,21 @@
+/* eslint no-underscore-dangle: ["error", { "allow": ["_id"] }] */
+
+const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const captureAsyncError = require('../utils/CaptureAsyncError');
 
 exports.signup = captureAsyncError(async (req, res, next) => {
   const newUser = await User.create(req.body);
 
+  const token = jwt.sign(
+    { id: newUser._id },
+    process.env.JWT_SECRET_KEY,
+    { expiresIn: process.env.JWT_EXPIRES_IN },
+  );
+
   return res.status(201).json({
     status: 'success',
+    token,
     user: newUser,
   });
 });
