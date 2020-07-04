fe7e79527f08bf3f8ed3b48644447acd9ec32641
Rujal Shrestha <rujal4shrz@gmail.com>
2020-07-04 23:56:54 +0545

    Send JWT through cookie

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -16,6 +16,17 @@ const signToken = (id) => jwt.sign(
 
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
+  const cookieOptions = {
+    expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 3600 * 1000),
+    httpOnly: true,
+  };
+
+  if (process.env.NODE_ENV === 'production') {
+    cookieOptions.secure = true;
+  }
+
+  res.cookie('jwt', token, cookieOptions);
+  user.password = undefined; //eslint-disable-line
 
   res.status(statusCode).json({
     status: 'success',
