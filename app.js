f205af2a83e1ca926de3095f22d9604196b7e18c
Rujal Shrestha <rujal4shrz@gmail.com>
2020-07-05 13:32:36 +0545

    Set rate limiter and security HTTP headers

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 const {
   TOURS_API,
   USERS_API,
@@ -11,11 +13,21 @@ const userRouter = require('./Routes/userRoutes');
 
 const app = express();
 
+app.use(helmet());
 app.use(express.static(`${__dirname}/public`));
-app.use(express.json());
+app.use(express.json({ limit: '10kb' }));
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
+
+app.use('/api', limiter);
+
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
