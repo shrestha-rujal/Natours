17b546facb399e154dc5aebce5194044d69f8b27
Rujal Shrestha <rujal4shrz@gmail.com>
2020-07-05 14:01:00 +0545

    Prevent paramter pollution with HPP

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
+const hpp = require('hpp');
 const {
   TOURS_API,
   USERS_API,
@@ -20,6 +21,16 @@ app.use(express.static(`${__dirname}/public`));
 app.use(express.json({ limit: '10kb' }));
 app.use(mongoSanitize());
 app.use(xss());
+app.use(hpp({
+  whitelist: [
+    'duration',
+    'ratingsQuantity',
+    'ratingsAverage',
+    'maxGroupSize',
+    'difficulty',
+    'price',
+  ],
+}));
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
