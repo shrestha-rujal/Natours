837bd8cfd7bb4a66cd69d7f1fce0261d013006cd
Rujal Shrestha <rujal4shrz@gmail.com>
2020-08-15 15:24:55 +0545

    Setup pug

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
@@ -18,8 +19,11 @@ const reviewRouter = require('./Routes/reviewRoutes');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
 app.use(helmet());
-app.use(express.static(`${__dirname}/public`));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json({ limit: '10kb' }));
 app.use(mongoSanitize());
 app.use(xss());
@@ -48,6 +52,10 @@ app.use('/api', limiter);
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
+});
+
+app.get('/', (req, res) => {
+  res.status(200).render('base');
 });
 
 app.use(TOURS_API, tourRouter);
