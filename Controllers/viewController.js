a4ad3106fb061c7c20f6bde89d0b62c10b7261d1
Rujal Shrestha <rujal4shrz@gmail.com>
2020-08-16 14:47:54 +0545

    Add Overview page UI

--- Controllers/viewController.js
+++ Controllers/viewController.js
@@ -1,8 +1,14 @@
-exports.getOverview = (req, res) => {
+const Tour = require('../models/tourModel');
+const captureAsyncError = require('../utils/CaptureAsyncError');
+
+exports.getOverview = captureAsyncError(async (req, res) => {
+  const tours = await Tour.find();
+
   res.status(200).render('overview', {
     title: 'All tours',
+    tours,
   });
-};
+});
 
 exports.getTour = (req, res) => {
   res.status(200).render('tour', {
