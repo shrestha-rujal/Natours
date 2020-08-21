53a57f73bc160e567eed59c00bc6d985ff2793a7
Rujal Shrestha <rujal4shrz@gmail.com>
2020-08-21 16:00:06 +0545

    Send real email in production mode using Sendgrid

--- utils/email.js
+++ utils/email.js
@@ -13,8 +13,13 @@ module.exports = class Email {
 
   createNewTransport() {
     if (process.env.NODE_ENV === 'production') {
-      //sendgrid
-      return 1;
+      return nodemailer.createTransport({
+        service: 'SendGrid',
+        auth: {
+          user: process.env.SENDGRID_USERNAME,
+          pass: process.env.SENDGRID_PASSWORD,
+        },
+      });
     }
 
     return nodemailer.createTransport({
