89c101fc771f4c8813bc22aed4174d0269834827
Rujal Shrestha <rujal4shrz@gmail.com>
2020-08-22 13:03:28 +0545

    Document tour booking on successful checkout

--- Controllers/bookingController.js
+++ Controllers/bookingController.js
@@ -1,13 +1,14 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const captureAsyncError = require('../utils/CaptureAsyncError');
 const Tour = require('../models/tourModel');
+const Booking = require('../models/bookingModel');
 
 exports.getCheckoutSession = captureAsyncError(async (req, res, next) => {
   const tour = await Tour.findById(req.params.tourId);
 
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
-    success_url: `${req.protocol}://${req.get('host')}`,
+    success_url: `${req.protocol}://${req.get('host')}?tour=${req.params.tourId}&user=${req.user.id}&price=${tour.price}`,
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourId,
@@ -27,4 +28,13 @@ exports.getCheckoutSession = captureAsyncError(async (req, res, next) => {
     status: 'success',
     session,
   });
+});
+
+exports.createBookingCheckout = captureAsyncError(async (req, res, next) => {
+  const { tour, user, price } = req.query;
+
+  if (!tour && !user && !price) return next();
+  await Booking.create({ tour, user, price });
+
+  return res.redirect(req.originalUrl.split('?')[0]);
 });
--- Routes/viewRoutes.js
+++ Routes/viewRoutes.js
@@ -3,8 +3,14 @@ const express = require('express');
 const router = express.Router();
 const viewController = require('../Controllers/viewController');
 const authController = require('../Controllers/authController');
+const bookingController = require('../Controllers/bookingController');
 
-router.get('/', authController.isUserOnline, viewController.getOverview);
+router.get(
+  '/',
+  bookingController.createBookingCheckout,
+  authController.isUserOnline,
+  viewController.getOverview,
+);
 router.get('/tour/:slug', authController.isUserOnline, viewController.getTour);
 router.get('/login', authController.isUserOnline, viewController.getLoginForm);
 router.get('/account', authController.checkLoggedIn, viewController.getAccount);
