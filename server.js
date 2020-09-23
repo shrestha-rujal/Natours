bde95f50decda107e7c598f5fb6bb8c65480e49a
root <root@ip-172-31-83-210.ec2.internal>
2020-09-23 07:01:34 +0000

    AWS setup

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+const fs = require('fs');
 
 process.on('uncaughtException', (err) => {
   console.error(err.name, err.message);
@@ -9,18 +10,35 @@ process.on('uncaughtException', (err) => {
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
-
-const DB_URI = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
 const UNHANDLED_REJECTION = 'unhandledRejection';
 const PORT = 3000;
 
-mongoose.connect(DB_URI, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true,
-})
-  .then(() => console.log('DATABASE CONNECTION SUCCESSFUL'));
+if (process.env.NODE_ENV === 'aws') {
+  const DB_URI = process.env.AWS_DATABASE.replace('<PASSWORD>', process.env.AWS_PASSWORD);
+  const CA = [fs.readFileSync("rds-combined-ca-bundle.pem")];
+  
+  mongoose.connect(DB_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+    sslValidate: true,
+    sslCA: CA, 
+  })
+    .then(() => console.log('AWS DB CONNECTION SUCCESSFUL'))
+    .catch((error) => console.log('AWS ERROR: ', error));
+
+} else {
+  const DB_URI = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+  
+  mongoose.connect(DB_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+    .then(() => console.log('DATABASE CONNECTION SUCCESSFUL'));
+}
 
 const server = app.listen(process.env.PORT || PORT, () => {
   console.log(`SERVER LISTENING AT ${PORT}`);
